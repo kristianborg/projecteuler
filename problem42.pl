b2b25a606140061a1d867ead1765e496e4d5ba44
Kristian Borg <gewoonkris@gmail.com>
2013-09-02 13:04:00 +0200

    add problem 42

---
+++ problem42.pl
@@ -0,0 +1,56 @@
+use FileHandle;
+
+my $file = "problem42.txt";
+my $fh = FileHandle->new;
+if (!$fh->open("< $file")) {
+	print "failed to open file\n";
+	exit;
+}
+my $wordString = <$fh>; 
+$fh->close;
+
+$wordString =~ s/"//g;
+my @words = split(",", $wordString);
+print "read ", scalar @words, " words\n";
+
+my $maxWordLength = getMaxLength(\@words);
+print "maxWordLength: $maxWordLength\n";
+
+my $maxTriangleNum = $maxWordLength * 26;
+@triangleNums = getTriangleNums($maxTriangleNum);
+print "number of triangle numbers untill $maxWordLength * 26 = $maxTriangleNum: ", scalar @triangleNums, "\n";
+
+my $result = 0;
+foreach(@words){
+	$sum = 0;
+	$sum += ord()-64 foreach(split("", $_));
+	if ($sum ~~ @triangleNums){
+		print "triangular: $_ = $sum\n";
+		$result++;
+	}
+}
+print "result: $result\n";
+
+sub  getTriangleNums(){
+	my $max = $_[0];
+	@result = ();
+	for(my $i = 1;;$i++){
+		$triangleNum = 0.5 * $i * ($i + 1);
+		if ($triangleNum > $max){
+			last;
+		}
+		push(@result, $triangleNum);
+	}
+	return @result;
+}
+
+sub getMaxLength(){
+	my @words = @{$_[0]};
+	$maxLength = 0;
+	foreach(@words){
+		if (length $_ > $maxLength){
+			$maxLength = length $_;
+		}
+	}
+	return $maxLength;
+}
