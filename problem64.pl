2597b3a818d5b83ee725f9a48a4b718df566b329
Kristian Borg <gewoonkris@gmail.com>
2013-10-05 16:13:02 +0200

    improve problem 64

--- problem64.pl
+++ problem64.pl
@@ -6,7 +6,7 @@ my $result = 0;
 for (my $i = 2; $i <= $max; $i++){
 	if (!isSquare($i)){
 		my $period = getFractionPeriod($i);
-		if ($period %2 == 1){
+		if ($period %2){
 			$result++;
 		}
 	}
@@ -19,9 +19,7 @@ sub getFractionPeriod(){
 	my $m = 0;
 	my $d = 1;
 	my $a = $sSqrt;
-	my @fractions = ($a);
-	my @ids = ($m . "-" . $d . "-" . $a);
-	#print "m=$m, d=$d, a=$a\n";
+	my @fractions = ();
 	while(1){
 		my $mNew = $d * $a - $m;
 		my $dNew = ($S - $mNew ** 2) / $d;
@@ -30,23 +28,12 @@ sub getFractionPeriod(){
 		$m = $mNew;
 		$d = $dNew;
 		$a = $aNew;
-		my $id = $m . "-" . $d . "-" . $a;
-		#print "m=$m, d=$d, a=$a\n";
-		if ($id ~~ @ids){
-			my @head = ();
-			my @tail = ();
-			for(my $i = 0; $i < @ids; $i++){
-				if($ids[$i] == $id || scalar(@tail)){
-					push(@tail, $fractions[$i]);
-				} else {
-					push(@head, $fractions[$i]);
-				}
-			}
-			#print "sqrt($S)=[@head; (@tail)], period=",scalar @tail, "\n";
-			return scalar @tail;
-		}
 		push(@fractions, $a);
-		push(@ids, $id);
+		if ($a == 2 * $sSqrt){
+			my $period = scalar @fractions;
+			#print "sqrt($S)=[$sSqrt; (@fractions)], period=$period\n";
+			return $period;
+		}
 	}
 }
 
