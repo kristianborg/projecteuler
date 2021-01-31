708b5754bc32634db1c73aa816f1de8e0b45208f
Kristian Borg <gewoonkris@gmail.com>
2021-01-31 17:08:25 +0100

    add Problem108

---
+++ src/main/java/nl/krisborg/projecteuler/problems/Problem108.java
@@ -0,0 +1,69 @@
+package nl.krisborg.projecteuler.problems;
+
+import java.math.BigDecimal;
+
+import static java.math.MathContext.DECIMAL128;
+
+public class Problem108 {
+
+    private void run(int targetDistinctSolutions) {
+        for(int n = 4;;n++) {
+            int numFactors = getNumFactors(n);
+            if (numFactors > targetDistinctSolutions) {
+                System.out.println("Done. Result: " + n);
+                return;
+            }
+        }
+    }
+
+    private int  getNumFactors(long n) {
+        int numSolutions = 0;
+        for (long x = n + 1;;  x++) {
+
+            /*
+             * We have 1/n.
+             * We rewrite this as ?/x by multiplying numerator and denominator by x/n:
+             * 1. 1/n = (x/n) / (n * (x/n)) = (x/n) / x
+             *
+             * We assume soltion 1 is 1/x.
+             * We try to find the other solution by subtracting 1/x:
+             * 2. (x/n) / x - 1/x = (x/n) / x - (n/n) / x = ((x-n) / n) / x.
+             *
+             * Now we rewrite this by myltiplying  numerator and denominator by n:
+             * 3. (((x-n) / n) / x) * n = (x-n) / (x*n)
+             *
+             * This solution is only acceptable if it can be rewritten as 1/y.
+             * That can only happen if (x*n) divides (x-n). So we check:
+             * 4. (x*n) % (x-n) == 0
+             *
+             * If this is the case, we have found the second solution
+             * 5 .y = (x-n) / (x*n)
+             */
+
+            // we start by the check from formula (4):
+            if (((x * n) % (x - n)) == 0) {
+                // now we can get y with formalue (5)
+                long y =  (x*n) / (x-n);
+                System.out.println(String.format("1/%d = 1/%d + 1/%d", n, x, y));
+                numSolutions++;
+
+                // we keep going untill 1/x < 1/y,
+                BigDecimal solution1 = new BigDecimal(1, DECIMAL128).divide(new BigDecimal(x), DECIMAL128);
+                BigDecimal solution2 = new BigDecimal(1, DECIMAL128).divide(new BigDecimal(y), DECIMAL128);
+                if (solution1.compareTo(solution2) <= 0) {
+                    if (x > 2*n) {
+                        new String();
+                    }
+                    break;
+                }
+            }
+        }
+        //System.out.println(String.format("Total number of solutions for %d: %d", n, numSolutions));
+        //System.out.println();
+        return numSolutions;
+    }
+
+    public static void main(String[] args) {
+        new Problem108().run(1000);
+    }
+}
